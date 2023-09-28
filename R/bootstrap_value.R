130c802010a2435f6d69c9489971f9ee5ea903a8
Jens Åström <jens.astrom@nina.no>
2023-09-28 14:55:30 +0200

    impl. rolling 5 year window

--- R/bootstrap_value.R
+++ R/bootstrap_value.R
@@ -3,6 +3,7 @@
 #' @param df dataframe (or tibble)
 #' @param value Column to bootstrap, "no_species" (default), "shannon_div", "mean_no_asv_per_species"
 #' @param groups Optional grouping variables as character vector.
+#' @param rolling_year_window Should the yearly values be calculated around a 5 year rolling window? Only applies if "year" is present in the groupings
 #' @param lower_limit Lower limit to confidence intervals, e.g. 0.025 for lower 2.5%
 #' @param upper_limit Upper limit to confidence intervals, e.g. 0.975 for upper 97.5%
 #' @param R Number of bootstrap samples
@@ -33,13 +34,33 @@
 bootstrap_value <- function(df,
                             value = c(no_species, shannon_div, mean_no_asv_per_species, sum_wet_weight, avg_wet_weight),
                             groups = NULL,
+                            rolling_year_window = TRUE,
                             lower_limit = 0.025,
                             upper_limit = 0.975,
                             R = 999){
 
+  if("locality" %in% groups & rolling_year_window) stop("Can't use rolling window with stratified by localities, they only occur once per rolling window")
+
+  #Expand dataset to include up to 5 year window around every year
+  if("year" %in% groups & rolling_year_window){
+
+    out <- NULL
+
+    for(subset_year in unique(df$year)){
+
+      subset <- df %>%
+        filter(year >= subset_year - 2 & year <= subset_year + 2) %>%
+        mutate(year_window = subset_year)
+
+      out <- rbind(out, subset)
+
+    }
+
+    df <- out
+    groups[groups == "year"] <- "year_window"
+    }
 
   groupings <- groups
-
 
   bootstrap_values <- df %>%
     group_by_at(groupings) %>%
